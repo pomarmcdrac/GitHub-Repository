862de731edd9f0ca1d5acfa047a2d348a495aa7b
Omar Morales <pomaral@live.com.mx>
2024-02-04 00:50:08 -0600

    Add showDialog to confirm launch url to see the changes on commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,52 @@
+// Import necessary packages and screens
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:github_repository/screens/screens.dart';
 
+// Entry point of the application
 void main() {
+  // Run the Flutter application
   runApp(const MyApp());
 }
 
+// Main application widget
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // Build method to define the structure of the application
   @override
   Widget build(BuildContext context) {
-
+    
+    // Set preferred device orientations to portrait
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
 
+    // Set system UI overlay style (status bar color)
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.black
     ));
     
+    // Return the MaterialApp widget, configuring the theme, initial route, and routes
     return MaterialApp(
       title: 'GitHub Repository',
       theme: ThemeData(
+        // Customize the color scheme for the application
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue[900]!),
+        
+        // Customize the AppBar theme
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black12,
         ),
+        
+        // Enable Material 3 theming
         useMaterial3: true,
       ),
+      // Disable the debug banner in the top-right corner
       debugShowCheckedModeBanner: false,
+      
+      // Set the initial route and define named routes with corresponding widgets
       initialRoute: HomeScreen.routeName,
       routes: {
         CommitsScreen.routeName: (context) => const CommitsScreen(),
--- lib/screens/commits_screen.dart
+++ lib/screens/commits_screen.dart
@@ -1,14 +1,16 @@
+// Importing necessary Dart packages and external libraries
 import 'dart:convert';
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:github_repository/models/models.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
+// List to store commit history
 List<CommitsModel> historyCommits = [];
 
+// Widget for displaying commit history screen
 class CommitsScreen extends StatefulWidget {
-
   static const String routeName = 'CommitsScreen';
 
   const CommitsScreen({Key? key}) : super(key: key);
@@ -17,10 +19,12 @@ class CommitsScreen extends StatefulWidget {
   State<CommitsScreen> createState() => _CommitsScreenState();
 }
 
+// State class for the CommitsScreen widget
 class _CommitsScreenState extends State<CommitsScreen> {
 
   @override
   void initState() {
+    // Fetch commits when the widget is initialized
     getCommits().then((commits) {
       setState(() {
         historyCommits = commits;
@@ -38,6 +42,7 @@ class _CommitsScreenState extends State<CommitsScreen> {
       child: Scaffold(
         backgroundColor: Colors.black87,
         appBar: AppBar(
+          // App bar with a back button and the title 'History of Commits'
           centerTitle: true,
           leading: IconButton(
             icon: const Icon(
@@ -63,12 +68,14 @@ class _CommitsScreenState extends State<CommitsScreen> {
           height: size.height,
           width: size.width,
           child: ListView.builder(
+            // List view to display commit history
             itemCount: historyCommits.length,
             padding: const EdgeInsets.all(10),
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 5),
                 child: Container(
+                  // Container for individual commit details
                   padding: const EdgeInsets.all(5),
                   decoration: BoxDecoration(
                     border: Border.all(
@@ -89,6 +96,7 @@ class _CommitsScreenState extends State<CommitsScreen> {
                   child: Column(
                     children: [
                       ListTile(
+                        // List tile for displaying commit author and message
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10),
                         ),
@@ -103,7 +111,7 @@ class _CommitsScreenState extends State<CommitsScreen> {
                           minFontSize: 12,
                         ),
                         subtitle: AutoSizeText(
-                          'Message: ${historyCommits[index].commit.message}',
+                          'Message: ${historyCommits[index].commit.message}. Tap the arrow to see more details.',
                           style: TextStyle(
                             color: Colors.grey[400],
                           ),
@@ -112,18 +120,20 @@ class _CommitsScreenState extends State<CommitsScreen> {
                           minFontSize: 8,
                         ),
                         trailing: IconButton(
+                          // Arrow icon button for launching URL
                           icon: Icon(
                             Icons.arrow_forward_ios_rounded, 
                             color: Colors.blueAccent[900],
                             size: 30,
                           ),
-                          onPressed: () => Navigator.of(context).pushNamed(
-                            CommitsScreen.routeName,
-                            arguments: historyCommits,
-                          ),
+                          onPressed: () {
+                            // Confirm and launch URL
+                            confirmLaunchUrl(context, historyCommits[index].htmlUrl, size);
+                          }
                         ),
                       ),
                       Container(
+                        // Container for displaying commit date and time
                         width: size.width,
                         padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
                         child: Row(
@@ -165,11 +175,12 @@ class _CommitsScreenState extends State<CommitsScreen> {
   }
 }
 
+// Function to fetch commit history from GitHub API
 Future getCommits() async {
-
   const String baseUrl = 'api.github.com';
   const String segmentUrl = '/repos/pomarmcdrac/GitHub-Repository/commits';
 
+  // Make a GET request to fetch commit data
   final response = await http.get(
     Uri.https(baseUrl, segmentUrl),
     headers: {
@@ -179,11 +190,82 @@ Future getCommits() async {
   );
 
   if (response.statusCode == 200) {
+    // If the request is successful, parse and return the list of commits
     final List<dynamic> commitsJson = jsonDecode(response.body);
     final List<CommitsModel> commits = commitsJson.map((json) => CommitsModel.fromJson(json)).toList();
     return commits;
   } else {
+    // If the request fails, throw an exception
     throw Exception('Failed to load commits');
   }
+}
 
+// Function to confirm and launch a URL
+void confirmLaunchUrl(BuildContext context, String url, Size size) async {
+  showDialog(
+    // Show a dialog with confirmation message
+    barrierDismissible: false,
+    context: context,
+    builder: (context) {
+      return Container(
+        decoration: const BoxDecoration(
+          color: Colors.black38,
+        ),
+        height: size.height * 0.2,
+        width: size.width * 0.8,
+        child: AlertDialog(
+          backgroundColor: Colors.black38,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(15),
+            side: const BorderSide(
+              color: Colors.grey,
+              width: 2,
+            )
+          ),
+          content: Material(
+            color: Colors.black38,
+            child: Container(
+              alignment: Alignment.center,
+              height: size.height * 0.15,
+              child: const Text(
+                'Do you want to proceed? This will take you out of the application to view the changes.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              // Cancel button
+              onPressed: () => Navigator.pop(context), 
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 12
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            TextButton(
+              // Continue button, launch URL
+              onPressed: () => launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication), 
+              child: const Text(
+                'Continue',
+                style: TextStyle(
+                  color: Colors.lightGreen,
+                  fontSize: 14
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
 }
